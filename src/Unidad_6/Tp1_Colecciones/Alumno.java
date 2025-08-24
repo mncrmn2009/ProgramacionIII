48d0caefb0c29ac938fed81587e8128f34e640c7
mncrmn2009 <mnc.rmn.2009@gmail.com>
2025-08-24 19:56:44 -0300

    Colecciones - Unidad 6

---
+++ src/Unidad_6/Tp1_Colecciones/Alumno.java
@@ -0,0 +1,40 @@
+package Unidad_6.Tp1_Colecciones;
+
+public class Alumno {
+    private String nombre;
+    private double nota;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, double nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "nombre='" + nombre + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+
+
+}
