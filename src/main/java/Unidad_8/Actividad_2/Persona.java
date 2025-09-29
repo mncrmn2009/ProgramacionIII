898fe5aaae272dcf09b6918385da17cbeeb79289
mncrmn2009 <mnc.rmn.2009@gmail.com>
2025-09-29 18:22:13 -0300

    unidad 8

---
+++ src/main/java/Unidad_8/Actividad_2/Persona.java
@@ -0,0 +1,26 @@
+package Unidad_8.Actividad_2;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    public void saludar (){
+        System.out.println("Hola");
+    }
+}
